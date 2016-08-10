143995f4e903d7906759ce9a2a36b43b43a2da74
jia-ming.chang <chang.jiaming@gmail.com>
2016-08-10 16:55:36 +0800

    pssm2tfpssm handle empty pssm file and feastures are set as 0

--- bin/source/pssm2tfpssm.cpp
+++ bin/source/pssm2tfpssm.cpp
@@ -12,6 +12,7 @@ bool fexists(string filename)
   return ifile;
 }
 
+
 int main(int argc, char *argv[])
 {
 	const int gappedDipeptides_dis=13;
@@ -21,26 +22,16 @@ int main(int argc, char *argv[])
 	string tfpssm_f = argv[2]; //output
 	Term term(gappedDipeptides_dis);
 
+
 	if(!fexists(tfpssm_f.c_str()))
 	{
+		
 		cout << "process  = " << pssm_f << endl;
 		cout << "generate = " << tfpssm_f << endl;
+
 		float *saveVector = new float [(int)term.size()];
-		
-		PSSM pssm(pssm_f.c_str());
-		//pssm.normal_smooth();
-		//pssm.window_smooth();
-		
-		for(i = 0; i < (int)term.size(); i++)
-		{
-			termWei = pssm.gap_dip_fre(term.get_term(i));
-			saveVector[i] = termWei;
-			weiSum += termWei;
-		}	
-		//Normalization
-		for(i = 0; i < (int)term.size(); i++)
-				saveVector[i] = saveVector[i]/weiSum;
-//output TFPSSM to csv file
+
+//open ouptut file		
 		fstream csvFile;
 		csvFile.open(tfpssm_f.c_str(), ios::out);
 		if(!csvFile)
@@ -48,14 +39,43 @@ int main(int argc, char *argv[])
 			perror(tfpssm_f.c_str());
 			return 0;
 		}
-		for(i = 0; i < (int)term.size(); i++)
-			csvFile << saveVector[i] << ",";
+
+//check whether PSSM is a empty file
+       std::ifstream file(pssm_f.c_str());
+       bool empty_pssm = ((!file) || (file.peek() == std::ifstream::traits_type::eof()));
+       if (file) 
+        file.close();
+       	
+	   if(empty_pssm){		
+	   		cout << "[WARNNING] " << pssm_f << " is empty or does not exist " << endl;	
+   			for(i = 0; i < (int)term.size(); i++)
+				csvFile << 0 << ",";
+	   }
+	   else{
+			PSSM pssm(pssm_f.c_str());
+			//pssm.normal_smooth();
+			//pssm.window_smooth();
+		
+			for(i = 0; i < (int)term.size(); i++)
+			{
+				termWei = pssm.gap_dip_fre(term.get_term(i));
+				saveVector[i] = termWei;
+				weiSum += termWei;
+			}	
+			//Normalization
+			for(i = 0; i < (int)term.size(); i++)
+					saveVector[i] = saveVector[i]/weiSum;
+	//output TFPSSM to csv file
+			for(i = 0; i < (int)term.size(); i++)
+				csvFile << saveVector[i] << ",";
+		}
+			
 		csvFile << endl;
 		csvFile.close();
 
 		delete [] saveVector;
-	}
-	else if(fexists(tfpssm_f))
+	}else if(fexists(tfpssm_f)){
 		cout << "tfpssm_file exist: " << tfpssm_f << endl;	
+	}
 }
 
